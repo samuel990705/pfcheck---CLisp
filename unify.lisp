a1f5f0c9ca6962b72e52ae74b6167159a12a1ac2
davidvalentine99 <60243651+davidvalentine99@users.noreply.github.com>
2021-06-02 21:03:26 -0500

    Update unify.lisp

--- unify.lisp
+++ unify.lisp
@@ -1,7 +1,8 @@
 ; unify.l carries out simple resolution. It is pretty much
 ; the code from AIP. 
 
-(dskin objects sets)
+load "objects.lisp" 
+load "sets.lisp"
 
 ; A unification is a data-type. POSLIT is the positive literal
 ; unified; NEGLIT, the negative literal; BDGS, the bindings
